852b52dc14f6ebd34f3b65be81215feb41527714
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-09-11 18:48:19 +0200

    Got food log queryset to break, change loggedfoodserializer to hold an actual foodserializer so i can get info from it. Might make it a bit more complex but we move

--- backend/caloriebalance/serializers.py
+++ backend/caloriebalance/serializers.py
@@ -2,18 +2,6 @@ from rest_framework import serializers
 from .models import LoggedFood, Food
 from datetime import date
 
-class LoggedFoodSerializer(serializers.ModelSerializer):
-    date_consumed = serializers.DateField(default=date.today)
-
-    class Meta:
-        model = LoggedFood
-        fields = ['id', 'food', 'quantity', 'date_consumed', 'calories_consumed']
-        read_only_fields = ['calories_consumed']
-    
-    def validate_food(self, value):
-        if self.instance and 'food' in self.initial_data:
-            raise serializers.ValidationError("You cannot change food field")
-        return value
 
 class FoodSerializer(serializers.ModelSerializer):
 
@@ -24,3 +12,15 @@ class FoodSerializer(serializers.ModelSerializer):
 
 
         
+class LoggedFoodSerializer(serializers.ModelSerializer):
+    date_consumed = serializers.DateField(default=date.today)
+    food = FoodSerializer(read_only=True)
+    class Meta:
+        model = LoggedFood
+        fields = ['id', 'food', 'quantity', 'date_consumed', 'calories_consumed']
+        read_only_fields = ['calories_consumed']
+    
+    def validate_food(self, value):
+        if self.instance and 'food' in self.initial_data:
+            raise serializers.ValidationError("You cannot change food field")
+        return value
