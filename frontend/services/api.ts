d6433035ee18603f0af9e1d6e7f9ef62b81568cd
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-10-05 21:21:23 +0200

    Before build

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -5,8 +5,8 @@ import * as SecureStore from 'expo-secure-store';
 const router = useRouter();
 
 export const TRACKER_CONFIG = {
-   //BASE_URL: 'https://calorie-tracker-gxq2.onrender.com',
-   BASE_URL: 'http://192.168.1.9:8000',
+   BASE_URL: 'https://calorie-tracker-gxq2.onrender.com',
+   //BASE_URL: 'http://192.168.1.9:8000',
    /* headers: { 
         accept: "application/json" ,
         Authorization: `Bearer ${getSecureItem()}`
